d6da1c3ff1f2f8c877689bb7e033968e13d5fc1a
Renê Furlanetto <renecreative13@gmail.com>
2021-03-02 16:52:58 -0300

    Begin Form

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="">
+        <p>Teste</p>
+        <input type="text" name="chupa">
+        <select name="aqui">
+            <option value="1">Teste 1</option>
+            <option value="2">Teste 2</option>
+            <option value="3">Teste 3</option>
+            <option value="4">Teste 4</option>
+        </select>
+
+        <button>Send</button>
+    </form>
+</body>
+</html>
